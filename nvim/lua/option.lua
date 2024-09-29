56319dd1353583d7a527677a829f578313af8778
Sohei Takasawa <stakasawa@SoheinoMac-mini.local>
2024-09-29 19:54:04 +0900

    Update: Refine settings in nvim/lua/option.lua

--- nvim/lua/option.lua
+++ nvim/lua/option.lua
@@ -1,22 +1,32 @@
 local set = vim.opt
 
-set.swapfile = false
-set.helplang = "ja"
-set.syntax = "on"
-set.encoding = "utf-8"
-set.title = true
-set.number = true
-set.ruler = true
-set.smartcase = true
-set.cursorline = true
-set.shiftwidth = 4
-set.tabstop = 4
 set.hls = true
-set.clipboard = "unnamed"
-set.showmatch = true
-set.fileencodings = "utf-8"
-set.updatetime = 300
-set.laststatus = 2
-set.expandtab = true
-set.autoindent = true
-set.smartindent = true
+set.ignorecase = true
+set.smartcase = true
+set.clipboard = 'unnamedplus'
+set.whichwrap = 'b,s,h,l,<,>,[,],~'
+set.encoding = 'utf-8'
+set.fileencodings = 'utf-8'
+
+if not vim.g.vscode then
+    set.swapfile = false
+    set.syntax = 'on'
+    set.updatetime = 300
+    set.title = true
+    set.number = true
+    set.ruler = true
+    set.cursorline = true
+    set.shiftwidth = 4
+    set.tabstop = 4
+    set.expandtab = true
+    set.showmatch = true
+    set.laststatus = 2
+    set.autoindent = true
+    set.smartindent = true
+    set.scrolloff = 6
+    set.list = true
+    set.listchars = { tab = '>-', trail = '-' }
+    set.completeopt = 'menuone'
+    set.termguicolors = true
+    set.incsearch = true
+end
